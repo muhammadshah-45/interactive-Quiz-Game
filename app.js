a450900abdaed8a8d4ba4b920e98d7e527077607
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-09-01 10:15:41 +0500

    I add questions in js file

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+
+//Create an array Object for answer
+let questions = [
+    {
+        question: "What is the best skills in Pakistan?",
+        options: ["Web Development", "Amazon", "Graphic Designing", "E-commerce"],
+        correct: 1
+    },
+    {
+        question: "Which one is beautiful city in Pakistan",
+        options: ["Peshawar", "Swat", "Murree", "Abottabad"],
+        correct: 2
+    },
+    {
+        question:"Why  we learn IT skills?",
+        options:["Earn Money","For Passion","For business","None of Them"],
+        correct: 1
+    },
+    {
+        question:"What is your goal in the future?",
+        options:["Skills Development","Education","Diplomas","Tourism"],
+        correct:1
+    }
+    
+];
+
